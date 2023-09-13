15c421d1c2c6dddacfc5284010c197c22056573a
ShinjiMC <nayapbrau@gmail.com>
2023-09-13 01:12:27 -0500

    Fixed Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import "../css/footer.css";
 
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+    <div className={`flex ${styles.flexStart} flex-col md:flex-row mb-8 w-full justify-between`}>
       <div className={`${styles.paddingX} flex-[1] flex flex-col mr-10 unsa-container`}>
         <img
           src={unsa}
@@ -18,7 +18,7 @@ const Footer = () => (
         </p>
       </div>
 
-      <div className={`${styles.paddingX} w-full footer-container`}>
+      <div className={`sm:px-20 px-6 flex flex-col  footer-container md:flex-row md:justify-between`}>
         {footerLinks.map((footerlink) => (
           <div
             key={footerlink.title}
@@ -46,7 +46,7 @@ const Footer = () => (
       </div>
     </div>
 
-    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+    <div className={`${styles.paddingX} w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]`}>
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
         Copyright Ⓒ 2023 School of Computer Science. All Rights Reserved.
       </p>
