86ada77008bcf01d52ba09294be7d40e803696fa
villanueva <villanueva.guerrero@outlook.com>
2023-09-30 14:15:27 -0500

    estoy cansado, jefe.

--- src/components/cards/InfoCard.jsx
+++ src/components/cards/InfoCard.jsx
@@ -2,10 +2,12 @@ import { features,socialMedia  } from "../../constants";
 import styles from "../../style";
 import {BsCode} from 'react-icons/bs';
 
-const InfoCard = ({ icon, title, content, index }) => (
-    <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+const InfoCard = ({ contentComponent: MyIcon, title, content, index }) => (
+    <section id = "card_container" className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
       <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
-        <BsCode className="featuredCard-icon"/>
+        {
+          MyIcon ? <MyIcon /> : <BsCode className="featuredCard-icon"/>
+        }
       </div>
       <div className="flex-1 flex flex-col ml-8">
         <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
@@ -15,7 +17,7 @@ const InfoCard = ({ icon, title, content, index }) => (
           {content}
         </p>
       </div>
-    </div>
+    </section>
 );
 
 export default InfoCard;
